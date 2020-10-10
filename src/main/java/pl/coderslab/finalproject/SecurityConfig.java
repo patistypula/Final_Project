cc25d3e44b663bf3b9b6ac9300b9925c35f86863
Patrycja StypuÅła <patistypula90@onet.eu>
2020-10-10 10:00:53 +0200

    10/10/20 css

--- src/main/java/pl/coderslab/finalproject/SecurityConfig.java
+++ src/main/java/pl/coderslab/finalproject/SecurityConfig.java
@@ -1,5 +1,6 @@
 package pl.coderslab.finalproject;
 
+import org.springframework.boot.autoconfigure.security.servlet.PathRequest;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
@@ -23,6 +24,7 @@ public class SecurityConfig extends WebSecurityConfigurerAdapter {
     @Override
     protected void configure(HttpSecurity http) throws Exception {
         http.authorizeRequests()
+                .requestMatchers(PathRequest.toStaticResources().atCommonLocations()).permitAll()
                 .antMatchers("/", "/landingPage").permitAll()
                 .anyRequest().authenticated()
 
